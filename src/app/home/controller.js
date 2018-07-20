853f19c6a47206f1a76de75a213d09d5fab37526
cyberpatriot <superness100@gmail.com>
2018-07-20 15:28:18 -0700

    hello

--- src/app/home/controller.js
+++ src/app/home/controller.js
@@ -1,6 +1,6 @@
 var app= angular.module('mainApp',[]);
 app.controller('people', function($scope, $http){
-    $http.get('http://localhost:4200/SocialFitWebsite/database.json')
+    $http.get('http://localhost:4200/SocialFitWebsite/home/database.json')
     .success(function(response){
         $scope.persons = response.records;
     });
